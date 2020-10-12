097703ce3a6fcfee52f83fac999cd854252d4aae
dragonet-d <Dragonet_D@163.com>
2020-10-12 10:36:10 +0800

    :pencial: add

--- src/pages/note/index.tsx
+++ src/pages/note/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import { NavLink, useRequest } from 'umi'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -7,42 +7,77 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Button from '@material-ui/core/Button'
-import { getList } from './service'
+import TextField from '@material-ui/core/TextField'
+import { getList, addItem } from './service'
+import styles from './index.scss'
 
 const Note = () => {
+  const [title, setTitle] = useState('')
+  const [link, setLink] = useState('')
+
   const { data } = useRequest(getList, {
     initialData: [],
     formatResult: res => res.data || [],
   })
 
+  // 添加
+  const { run: addItemRun } = useRequest(addItem, {
+    manual: true,
+  })
+
+  const handleAdd = () => {
+    addItemRun({
+      title,
+      url: link,
+    })
+  }
+
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>标题</TableCell>
-            <TableCell>链接</TableCell>
-            <TableCell>操作</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((i: any) => (
-            <TableRow key={i.id}>
-              <TableCell component="th" scope="row">
-                {i.title}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                <NavLink to={i.url}>{i.url}</NavLink>
-              </TableCell>
-              <TableCell>
-                <Button color="primary">编辑</Button>
-                <Button color="secondary">删除</Button>
-              </TableCell>
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <TextField
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          label="标题"
+        />
+        <TextField
+          value={link}
+          onChange={e => setLink(e.target.value)}
+          className={styles.link}
+          label="链接"
+        />
+        <Button onClick={handleAdd} color="primary" variant="contained">
+          确定
+        </Button>
+      </div>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>标题</TableCell>
+              <TableCell>链接</TableCell>
+              <TableCell>操作</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.map((i: any) => (
+              <TableRow key={i.id}>
+                <TableCell component="th" scope="row">
+                  {i.title}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  <NavLink to={i.url}>{i.url}</NavLink>
+                </TableCell>
+                <TableCell>
+                  <Button color="primary">编辑</Button>
+                  <Button color="secondary">删除</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   )
 }
 
--- src/pages/note/service.ts
+++ src/pages/note/service.ts
@@ -1,7 +1,16 @@
 import { request } from 'umi'
 
+// 获取列表
 export function getList() {
   return request('/api/note/getList', {
     method: 'get',
+  })
+}
+
+// 添加
+export function addItem(params: { title: string; url: string }) {
+  return request('api/note/add', {
+    method: 'post',
+    params,
   })
 }
