8832627765fd4cb78cf3a0901dab54b4436ab2a5
dragonet-d <Dragonet_D@163.com>
2021-03-30 21:48:19 +0800

    :art: update

--- src/pages/markdown/index.tsx
+++ src/pages/markdown/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Button } from '@material-ui/core'
 import { useRequest } from 'umi'
